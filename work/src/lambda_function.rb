28a7530e9ac6479efe950ed4ace5ca6fd4f137cb
Terry Brady <terrywbrady@gmail.com>
2020-07-10 16:06:42 -0700

    Update lambda_function.rb

--- work/src/lambda_function.rb
+++ work/src/lambda_function.rb
@@ -1,7 +1,18 @@
 require 'json'
 
+def format(obj, key)
+  return "" unless obj
+  return "" unless obj[key]
+  obj[key].to_json
+done
+
 def lambda_handler(event:, context:)
     # TODO implement
-    { statusCode: 200, body: event.to_json }
+    {
+      statusCode: 200,
+      body: {
+        path: format(event, 'path')
+      }
+    }
     #JSON.generate('Hello from Lambda!')
 end
