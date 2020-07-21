2f825ed64b3378af8051214a4a87a56a2a222895
Terry Brady <terrywbrady@gmail.com>
2020-07-21 16:47:06 -0700

    add ssm logic

---
+++ test/ssm.rb
@@ -0,0 +1,52 @@
+require 'yaml'
+require 'aws-sdk-ssm'
+
+# This code is designed to mimic https://github.com/terrywbrady/yaml/blob/master/config.yml
+
+@regex = "\\{!(ENV|SSM):\\s*([^\\}!]*)(!DEFAULT:\\s([^\\}]*))?\\}"
+@ssm = Aws::SSM::Client.new
+
+def resolveValues(file)
+  config = YAML.load_file(file)
+  resolveValue(config)
+end
+
+def resolveValue(obj)
+  if obj.instance_of?(Hash)
+    copy = Hash.new
+    obj.each do |k, v|
+      copy[k] = resolveValue(v)
+    end
+    copy
+  elsif obj.instance_of?(Array)
+    arr = Array.new
+    obj.each do |v|
+      arr.push(resolveValue(v))
+    end
+    arr
+  elsif obj.instance_of?(String)
+    val = obj
+    m = obj.match(@regex)
+    if m
+      type, key, x, defval = m.captures
+      key = key.strip if key
+      defval = defval.strip if defval
+      puts "#{type} #{key} #{defval}"
+      val = defval if defval
+      if type == 'ENV'
+        val = ENV[key] if ENV.key?(key)
+      elsif type == 'SSM'
+        begin
+          @ssm.get_parameter(name: key)[:parameter][:value]
+        rescue
+        end
+      end
+    end
+    val
+  else
+    obj
+  end
+end
+
+conf = resolveValues('config/database2.yml')
+puts(conf)
