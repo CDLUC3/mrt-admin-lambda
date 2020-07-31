9cd5318603da9dfe8fcd4567d57b7ebc4beab722
Terry Brady <terrywbrady@gmail.com>
2020-07-31 14:38:42 -0700

    eliminate iteration on cout objects

--- src/queries/count_objects_query.rb
+++ src/queries/count_objects_query.rb
@@ -8,26 +8,6 @@ class CountObjectsQuery < AdminQuery
   end
 
   def get_sql
-    if is_total
-      get_total_sql
-    else
-      get_group_sql
-    end
-  end
-
-  def get_iterative_sql
-    get_campus_and_total_query
-  end
-
-  def resolve_params
-    if is_total
-      []
-    else
-      [ @itparam1 ]
-    end
-  end
-
-  def get_total_sql
     %{
       select
         'ZZZ' as ogroup,
@@ -35,37 +15,28 @@ class CountObjectsQuery < AdminQuery
         sum(count_objects) as count_objects
       from
         owner_collections_objects
+
+      union
+
+      select
+        ogroup,
+        collection_name,
+        sum(count_objects) as count_objects
+      from
+        owner_collections_objects
+      group by
+        ogroup,
+        collection_name
+
+      union
+
+      select
+        ogroup,
+        '-- Total --' as collection_name,
+        sum(count_objects) as count_objects
+      from
+        owner_collections_objects
     }
-  end
-
-
-  def get_group_sql
-    if @itparam2.to_i == 1
-      %{
-        select
-          ogroup,
-          collection_name,
-          sum(count_objects) as count_objects
-        from
-          owner_collections_objects
-        where
-          ogroup = ?
-        group by
-          ogroup,
-          collection_name
-      }
-    else
-      %{
-        select
-          ogroup,
-          '-- Total --' as collection_name,
-          sum(count_objects) as count_objects
-        from
-          owner_collections_objects
-        where
-          ogroup = ?
-      }
-    end
   end
 
   def get_headers(results)
