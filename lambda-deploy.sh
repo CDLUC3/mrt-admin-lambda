5974554ae8b6d0b5e7aab56cc368c154ed1c2088
Terry Brady <terrywbrady@gmail.com>
2020-08-04 09:07:37 -0700

    update scripts

---
+++ lambda-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# If you have modified the Gemfile, you must run `makeDependencies.sh`
+
+# Depends on https://github.com/CDLUC3/uc3-aws-cli scripts
+EXIT_ON_DIE=true
+source ~/.profile.d/uc3-aws-util.sh
+
+# Check that the SSM_ROOT_PATH has been initialized
+check_ssm_root
+
+# Get the ARN for the lambda to publish
+LAMBDA_ARN=`get_ssm_value_by_name admintool/lambda-arn`
+
+# Get the URL for links to Merritt
+MERRITT_PATH=`get_ssm_value_by_name admintool/merritt-path`
+
+# Display zip contents to the user
+unzip -l deploy.zip
+
+# deploy lambda code
+aws lambda update-function-code --function-name ${LAMBDA_ARN} --zip-file fileb://deploy.zip --region us-west-2
+
+# Set environment and set timeout
+aws lambda update-function-configuration --function-name ${LAMBDA_ARN} --region us-west-2 --timeout 60 --memory-size 128 --environment "Variables={SSM_ROOT_PATH=${SSM_ROOT_PATH},MERRITT_PATH=${MERRITT_PATH}}"
