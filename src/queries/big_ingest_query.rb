992692635933f2a9f3dabd4e0397d358388ea616
Terry Brady <terrywbrady@gmail.com>
2020-11-10 16:40:30 -0800

    big query

--- src/queries/big_ingest_query.rb
+++ src/queries/big_ingest_query.rb
@@ -1,6 +1,14 @@
 class BigIngestQuery < AdminQuery
+  def initialize(query_factory, path, myparams)
+    super(query_factory, path, myparams)
+    @days = get_param('days', '14').to_i
+    @days = 365 if (@days > 365)
+    @items = get_param('items', '200').to_i
+    @items = 50 if (@items < 50)
+  end
+
   def get_title
-    "Big Ingests - Last Week"
+    "Big Ingests (#{@items}+) - Last #{@days} days"
   end
 
   def get_sql
@@ -15,16 +23,20 @@ class BigIngestQuery < AdminQuery
       where 
         batch_id != 'JOB_ONLY'
       and 
-        submitted >= date_add(now(), Interval -7 day)
+        submitted >= date_add(now(), Interval - ? day)
       group by 
         profile, 
         batch_id, 
         date(submitted) 
       having 
-        count(*) > 200
+        count(*) > ?
       order by 
         date(submitted) desc;
     }
+  end
+
+  def get_params
+    [@days, @items]
   end
 
   def get_headers(results)
@@ -32,7 +44,7 @@ class BigIngestQuery < AdminQuery
   end
 
   def get_types(results)
-    ['', '', '', 'dataint']
+    ['', 'batch', '', 'dataint']
   end
 
 end
