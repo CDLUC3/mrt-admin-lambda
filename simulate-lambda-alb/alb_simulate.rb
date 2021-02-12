a1fd20f5baea0008f8f696c022e59f446fb91da6
Terry Brady <terrence.brady@ucop.edu>
2021-02-12 08:52:46 -0800

    add POST support

--- simulate-lambda-alb/alb_simulate.rb
+++ simulate-lambda-alb/alb_simulate.rb
@@ -7,6 +7,9 @@ require 'sinatra/base'
 require 'json'
 require 'httpclient'
 
+require "base64"
+require "uri"
+
 # ruby app.rb -o 0.0.0.0
 set :port, 8091
 
@@ -49,13 +52,22 @@ end
 get '/lambda*' do
   path = params['splat'][0]
   path=path.gsub(/^lambda\//,'')
-  event = {path: path, queryStringParameters: params}.to_json
+  event = {
+    path: path, 
+    queryStringParameters: params,
+    httpMethod: "GET"
+  }.to_json
   lambda_process(event)
 end
 
 post '/lambda*' do
   path = params['splat'][0]
   path=path.gsub(/^lambda\//,'')
-  event = {path: path, queryStringParameters: params}.to_json
+  event = {
+    path: path, 
+    body: Base64.encode64(URI.encode_www_form(params)),
+    httpMethod: "POST",
+    isBase64Encoded: true
+  }.to_json
   lambda_process(event)
 end
