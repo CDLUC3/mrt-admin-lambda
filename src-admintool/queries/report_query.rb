d69a2f0e27d8c765233ec5701a93292ca1a6e8ad
Terry Brady <terrence.brady@ucop.edu>
2021-10-12 17:58:21 -0700

    add prior/next day nav

--- src-admintool/queries/report_query.rb
+++ src-admintool/queries/report_query.rb
@@ -3,18 +3,33 @@ class ReportRetrieve < AdminQuery
     super(query_factory, path, myparams)
     @day = Time.new.strftime('%Y-%m-%d')
     @report = CGI.unescape(get_param('report', "#{@s3consistency}#{@day}"))
+    @report_base = @report
+      .sub(@s3consistency, '')
+      .gsub(%r[(\d\d\d\d-\d\d-\d\d)/], '')
+      .gsub(%r[\.(PASS|INFO|WARN|FAIL|SKIP)$], '')
     m = @report.match(%r[#{@s3consistency}(\d\d\d\d-\d\d-\d\d)])
     @day = m[1] unless m.nil?
   end
 
   def get_title
-    "Retrive Report #{@report}"
+    "Retrive Report: #{@report}"
   end
 
   def run_sql
     json = get_report(@report)
-    return json unless json.nil?
-    super.run_sql
+    json = super.run_sql if json.nil?
+    json.fetch("alternative_queries", json.fetch(:alternative_queries, []))
+      .append(
+        {
+          label: "Prior Day - #{@report_base}", 
+          url: "path=report&report=#{@s3consistency}#{(Time.parse(@day) - 24*60*60).strftime('%Y-%m-%d')}/#{@report_base}"
+        },
+        {
+          label: "Next Day - #{@report_base}", 
+          url: "path=report&report=#{@s3consistency}#{(Time.parse(@day) + 24*60*60).strftime('%Y-%m-%d')}/#{@report_base}"
+        }  
+      )
+    return json
   end
 
   def is_saveable?
