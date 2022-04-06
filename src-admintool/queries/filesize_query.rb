f763962ec34bcdd9394bccaa73e53b67ddd26319
Terry Brady <terrence.brady@ucop.edu>
2022-04-05 17:42:42 -0700

    split file size report

--- src-admintool/queries/filesize_query.rb
+++ src-admintool/queries/filesize_query.rb
@@ -4,7 +4,7 @@ class FilesizeQuery < AdminQuery
   end
 
   def get_title
-    "File size summary"
+    "Producer File size summary"
   end
 
   def get_sql
@@ -13,19 +13,13 @@ class FilesizeQuery < AdminQuery
         max(billable_size) max_size,
         min(billable_size) min_size,
         avg(billable_size) avg_size,
-        count(*) num_files,
-        sum(if(billable_size < 1000, 1, 0)) num_under_1k,
-        sum(if(billable_size >= 1000 and billable_size < 1000000, 1, 0)) num_1k_to_1m,
-        sum(if(billable_size >= 1000000 and billable_size < 10000000, 1, 0)) num_1m_to_10m,
-        sum(if(billable_size >= 10000000 and billable_size < 100000000, 1, 0)) num_10m_to_100m,
-        sum(if(billable_size >= 100000000 and billable_size < 1000000000, 1, 0)) num_100m_to_1g,
-        sum(if(billable_size >= 1000000000 and billable_size < 10000000000, 1, 0)) num_1g_to_10g,
-        sum(if(billable_size >= 10000000000 and billable_size < 100000000000, 1, 0)) num_10g_to_100g,
-        sum(if(billable_size >= 100000000000, 1, 0)) num_100g_plus
+        count(*) num_files
       from
         inv.inv_files
       where
         source='producer'
+      and
+        billable_size = full_size
       ;
     }
   end
@@ -35,15 +29,7 @@ class FilesizeQuery < AdminQuery
       'Max Size',
       'Min Size',
       'Avg Size',
-      'Num files',
-      'Num (<1K)',
-      'Num (1K-1M)',
-      'Num (1M-10M)',
-      'Num (10M-100M)',
-      'Num (100M-1G)',
-      'Num (1G-10G)',
-      'Num (10G-100G)',
-      'Num (>100G)'
+      'Num files'
    ]
   end
 
@@ -51,15 +37,7 @@ class FilesizeQuery < AdminQuery
     [
       'bytes', 
       'bytes', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
-      'dataint', 
+      'bytes', 
       'dataint'
     ]
   end
