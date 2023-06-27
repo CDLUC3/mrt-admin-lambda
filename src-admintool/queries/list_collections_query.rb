20855dd628f16d604babd523d8ede8b9c0316501
Terry Brady <terrence.brady@ucop.edu>
2023-06-27 10:52:23 -0700

    list collections

---
+++ src-admintool/queries/list_collections_query.rb
@@ -0,0 +1,33 @@
+class ListCollectionsQuery < AdminQuery
+  def initialize(query_factory, path, myparams)
+    super(query_factory, path, myparams)
+  end
+
+  def get_title
+    "List Collections"
+  end
+
+  def get_sql
+    %{
+      select
+        ogroup,
+        inv_collection_id,
+        mnemonic,
+        collection_name
+      from
+        owner_collections
+      order by
+        ogroup,
+        mnemonic
+    }
+  end
+
+  def get_headers(results)
+    ['Group', 'CollId', 'mnemonic', 'Collection']
+  end
+
+  def get_types(results)
+    ['ogroup', 'colllist', 'mnemonic', 'name']
+  end
+
+end
