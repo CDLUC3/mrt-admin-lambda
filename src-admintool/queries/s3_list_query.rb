6b696c3c0ef44939e8e6bfebc3198d75e1aed5e4
Terry Brady <terrence.brady@ucop.edu>
2024-02-06 13:02:31 -0800

    daily build output

--- src-admintool/queries/s3_list_query.rb
+++ src-admintool/queries/s3_list_query.rb
@@ -7,28 +7,46 @@ class S3ListQuery < AdminQuery
   def initialize(query_factory, path, myparams)
     super(query_factory, path, myparams)
     rptpath = myparams.fetch('rptpath', 'daily-build')
+    status_file = myparams.fetch('status-file', '')
     @report = "merritt-reports/#{rptpath}"
-    @files = [] 
+    @files = []
+    @status = 'SKIP'
+
+    begin
+      unless status_file.empty?
+        resp = @s3_client.get_object({
+          bucket: @s3bucket,
+          key: "#{@report}/#{status_file}"
+        })
+        @status = resp.body.read.chop
+      end
+    rescue StandardError
+      LambdaBase.log_config(config, "#{@report}/#{status_file} does not exist")
+    end
+
     resp = @s3_client.list_objects_v2({
       bucket: @s3bucket,
       prefix: @report
     })
     resp.contents.each do |s3obj|
       link = "#{s3obj.key};#{get_report_url(s3obj.key)}"
-      @files.push([link, s3obj.last_modified, s3obj.size])
+      @files.push([link, s3obj.last_modified, s3obj.size, @status])
     end
   end
 
   def get_headers
-    ['File', 'Modified', 'Size']
+    %w[File Modified Size Status]
   end
 
   def get_types
-    ['link', 'datetime', 'bytes']
+    %w[link datetime bytes status]
   end
 
   def run_sql
     return_data(@files, get_types, get_headers)
   end
 
+  def init_status
+    :PASS
+  end
 end
