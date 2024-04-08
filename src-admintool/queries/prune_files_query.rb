78409116abc42b98522bdb1b9d6d3a577ce43d03
Terry Brady <terrence.brady@ucop.edu>
2024-04-08 16:24:28 -0700

    rubocop

--- src-admintool/queries/prune_files_query.rb
+++ src-admintool/queries/prune_files_query.rb
@@ -14,80 +14,80 @@ class PruneCandidateFilesQuery < AdminQuery
   def get_sql
     %{
     select distinct
-      o.ark, 
-      o.version_number, 
+      o.ark,
+      o.version_number,
       substring_index(f.pathname, '?', 1) norm,
       max(v.number),
       count(distinct f.pathname),
       count(distinct digest_value)
-    from 
-      inv.inv_objects o 
+    from
+      inv.inv_objects o
     inner join
-      inv.inv_files f 
-    on 
-      f.inv_object_id = o.id 
+      inv.inv_files f
+    on
+      f.inv_object_id = o.id
     inner join
       inv.inv_versions v
-    on 
+    on
       f.inv_version_id = v.id
     where exists (
-      select 1 
-      from 
+      select 1
+      from
         inv.inv_collections_inv_objects icio
-      inner join 
-        inv.inv_collections c 
-      on 
-        c.id = icio.inv_collection_id  
-      and 
+      inner join
+        inv.inv_collections c
+      on
+        c.id = icio.inv_collection_id
+      and
         c.mnemonic= ?
       where
         icio.inv_object_id=o.id
     )
-    and 
+    and
       f.pathname like 'producer/%'
-    group by 
+    group by
       ark,version_number,norm
-    having 
+    having
       count(distinct f.pathname) > 1
-    and 
+    and
       count(distinct digest_value) > 1
     union
     select distinct
-      o.ark, 
-      o.version_number, 
+      o.ark,
+      o.version_number,
       substring_index(f.pathname, '?', 1) norm,
       max(v.number),
       count(distinct f.pathname),
       count(distinct digest_value)
-    from 
-      inv.inv_objects o 
+    from
+      inv.inv_objects o
     inner join
-       inv.inv_files f 
-    on 
-      f.inv_object_id = o.id 
+       inv.inv_files f
+    on
+      f.inv_object_id = o.id
     inner join
       inv.inv_versions v
-    on 
+    on
       f.inv_version_id = v.id
     where exists (
-      select 1 
-      from 
+      select 1
+      from
         inv.inv_collections_inv_objects icio
-      inner join 
-        inv.inv_collections c 
-      on 
-        c.id = icio.inv_collection_id  
-      and 
+      inner join
+        inv.inv_collections c
+      on
+        c.id = icio.inv_collection_id
+      and
         c.mnemonic= ?
       where
        icio.inv_object_id=o.id
     )
-    and 
+    and
       f.pathname like 'producer/%'
-    group by 
+    group by
       ark,version_number,norm
-    having 
-      count(distinct f.pathname) = 1 
+    having
+      count(distinct f.pathname) = 1
     and
       max(v.number) < o.version_number;
     }
