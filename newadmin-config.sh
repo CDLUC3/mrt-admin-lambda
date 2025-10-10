dbd50364dedeadd0e7d5115e961e7112a21a3e21
Terry Brady <terrence.brady@ucop.edu>
2025-10-10 16:10:01 -0700

    scripts to deploy new admin tool over old

---
+++ newadmin-config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DEPLOY_ENV=${1:-dev}
+
+EXIT_ON_DIE=true
+source ~/.profile.d/uc3-aws-util.sh
+
+# Check that the SSM_ROOT_PATH has been initialized
+check_ssm_root
+
+# Assume deploy runs from DEV
+# Set ENV based on deploy env
+SSM_DEPLOY_PATH=${SSM_ROOT_PATH//dev/${DEPLOY_ENV}}
+AWS_ACCOUNT_ID=`aws sts get-caller-identity| jq -r .Account` || die "AWS Account Not Found"
+FUNCTNAME=uc3-mrt-admin-lambda
+
+# Get the ARN for the lambda to publish
+LAMBDA_ARN_BASE=arn:aws:lambda:${AWS_REGION}:${AWS_ACCOUNT_ID}:function:uc3-mrt-admintool-img
+LAMBDA_ARN=${LAMBDA_ARN_BASE}-${DEPLOY_ENV}
+
+if [ "${DEPLOY_ENV}" == "prd" ]
+then
+  REP=
+else
+  REP=-${DEPLOY_ENV}
+fi
+
+ADMIN_ALB_URL=`get_ssm_value_by_name admintool/api-path`
+ADMIN_ALB_URL=${ADMIN_ALB_URL//-dev/${REP}}
+ECS_URL=`get_ssm_value_by_name admintool/ecs-${DEPLOY_ENV}`
+
+COLLADMIN_ALB_URL=`get_ssm_value_by_name colladmin/api-path`
+COLLADMIN_ALB_URL=${COLLADMIN_ALB_URL//-dev/${REP}}
+
+VARS="RACK_CONFIG=app/config_uc3.ru,MERRITT_ECS=lambda,SSM_ROOT_PATH=/uc3/mrt/dev/"
+
+aws lambda update-function-configuration \
+  --function-name ${LAMBDA_ARN} \
+  --region us-west-2 \
+  --output text \
+  --timeout 360 \
+  --memory-size 2500 \
+  --ephemeral-storage "Size=2048" \
+  --no-cli-pager \
+  --environment "Variables={${VARS}}" 
+
+date
